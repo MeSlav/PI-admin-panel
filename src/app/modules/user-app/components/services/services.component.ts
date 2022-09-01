2cfbb1f23b59eec9ae6e74c0b9f6e1f959722d5a
MeSlav <mislav.kordic@gmail.com>
2022-09-02 00:05:53 +0200

    Remove unnecessary class and implement layout for services view

--- src/app/modules/user-app/components/services/services.component.ts
+++ src/app/modules/user-app/components/services/services.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Employee } from 'src/app/types/types';
+import { EquipmentService } from '../../services/equipment.service';
 
 @Component({
   selector: 'app-services',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent implements OnInit {
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor() { }
+  equipment: MatTableDataSource<Employee> = new MatTableDataSource();
+  displayedColumns: string[] = ['id', 'name', 'market_value', 'price_per_hour', 'category'];
+
+  constructor(private employeesService: EquipmentService) { }
 
   ngOnInit(): void {
+    this.employeesService.getEquipment()
+      .subscribe((res) => {
+        this.equipment.data = (res as any).results as Employee[];
+      });
+  }
+
+  ngAfterViewInit(): void {
+    this.equipment.sort = this.sort;
+    this.equipment.paginator = this.paginator;
+  }
+
+  applyFilter(e: Event) {
+    const filterValue = (e.target as HTMLInputElement).value;
+    this.equipment.filter = filterValue.trim().toLowerCase();
+
+    if (this.equipment.paginator) {
+      this.equipment.paginator.firstPage();
+    }
   }
 
 }
